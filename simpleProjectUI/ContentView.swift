2e4002ca37debf38795c5cd1650584d7e104fb25
Adham <adam.bolotov@gmail.com>
2022-02-19 07:02:42 +0100

    simlpPrUI

--- simpleProjectUI/ContentView.swift
+++ simpleProjectUI/ContentView.swift
@@ -1,16 +1,90 @@
-//
-//  ContentView.swift
-//  simpleProjectUI
-//
-//  Created by user213083 on 2/19/22.
-//
+
 
 import SwiftUI
 
+
+//
+
+
+
+class ViewModel: ObservableObject {
+    
+    @Published var image: Image?
+    
+    
+    func fetchNewImage() {
+        guard let url = URL(string: "https://random.imagecdn.app/500/500") else {
+            return
+        }
+        
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else {return}
+            
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data) else {
+                    return
+                }
+                self.image = Image(uiImage: uiImage)
+                
+            }
+        }
+        task.resume()
+    }
+}
+
+
+
 struct ContentView: View {
+    
+   @StateObject var viewModel = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .center){
+                
+                Spacer()
+                
+                if let image = viewModel.image {
+                    ZStack{
+                        image
+                            .resizable()
+                            .foregroundColor(Color.gray)
+                            .frame(width: 300, height: 300)
+                            .padding()
+                            //.cornerRadius(8)
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 1.2, height: UIScreen.main.bounds.width / 1.2)
+                    .background(Color.gray)
+                    .cornerRadius(8)
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .foregroundColor(Color.gray)
+                        .frame(width: 200, height: 200)
+                        .padding()
+                }
+                
+                Spacer()
+                
+                Button {
+                    viewModel.fetchNewImage()
+                } label: {
+                    Text("New Image!")
+                        .bold()
+                        .frame(width: 250, height: 55)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding()
+                }
+
+                
+            }
+            .navigationTitle("Photo Mania")
+            
+            
+        }
     }
 }
 
