1b5a1e07214b049338a61eaf06a8a5144670fcf4
Adham <adam.bolotov@gmail.com>
2022-02-19 06:16:09 +0100

    Initial Commit

---
+++ simpleProjectUI/simpleProjectUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleProjectUIApp.swift
+//  simpleProjectUI
+//
+//  Created by user213083 on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct simpleProjectUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
